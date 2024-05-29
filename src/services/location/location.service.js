dac69bad8b07e28353b01f74d234622a138cd9cb
JayChevli18 <chevlijay70@gmail.com>
2024-05-29 09:53:08 +0530

    Firebase Setup - Geocode

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,18 +1,26 @@
 import camelize from "camelize";
-import { locations } from "./location.mock";
+//import { locations } from "./location.mock";
 
+
+// export const locationRequest=(searchTerm)=>{
+//     return new Promise((resolve,reject)=>{
+//         const locationMock=locations[searchTerm];
+//         if(!locationMock)
+//             reject("Not Found");
+//         resolve(locationMock);
+//     });
+// };
 
 export const locationRequest=(searchTerm)=>{
-    return new Promise((resolve,reject)=>{
-        const locationMock=locations[searchTerm];
-        if(!locationMock)
-            reject("Not Found");
-        resolve(locationMock);
+    return fetch(
+        `http://192.168.29.214:5001/foodapp-424112/us-central1/geocode?city=${searchTerm}`
+    ).then((res)=>{
+        return res.json();
     });
 };
 
-
 export const locationTransform=(result)=>{
+    console.log(result);
     const formattedResponse=camelize(result);
     const {geometry={}}=formattedResponse.results[0];
     const {lat,lng}=geometry.location;
